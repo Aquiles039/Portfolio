727d36c339f593586045a268b2a44f682f0c79f4
Aquiles039 <aquiles_039@hotmail.com>
2024-11-19 21:47:41 +0100

    Add files via upload

---
+++ Companies_Filter.py
@@ -0,0 +1,58 @@
+import yfinance as yf
+
+Stock_in_criteria = []
+
+def less_than(stocks, price_goal):
+    for stock in stocks:
+        global Stock_in_criteria
+        stock_price = yf.Ticker(str(stock)).info['currentPrice']
+        if stock_price < price_goal:
+            Stock_in_criteria.append(stock)
+    return Stock_in_criteria
+
+def more_than(stocks, price_goal):
+    for stock in stocks:
+        global Stock_in_criteria
+        stock_price = yf.Ticker(str(stock)).info['currentPrice']
+        if stock_price > price_goal:
+            Stock_in_criteria.append(stock)
+    return Stock_in_criteria
+
+def between(stocks, low_price, high_price):
+    for stock in stocks:
+        global Stock_in_criteria
+        stock_price = yf.Ticker(str(stock)).info['currentPrice']
+        if (stock_price > low_price) & (stock_price< high_price):
+            Stock_in_criteria.append(stock)
+    return Stock_in_criteria
+
+def in_industry(stocks, industry):
+    for stock in stocks:
+        global Stock_in_criteria
+        try:
+            company_industry = yf.Ticker(str(stock)).info['industry']
+            if company_industry.upper() == industry.upper():
+                Stock_in_criteria.append(stock)
+        except:
+            company_industry = "Null"
+
+    return Stock_in_criteria
+
+def comp_currency(stocks, currency):
+    for stock in stocks:
+        global Stock_in_criteria
+        try:
+            company_industry = yf.Ticker(str(stock)).info['currency']
+            if company_industry.upper() == currency.upper():
+                Stock_in_criteria.append(stock)
+        except:
+            currency = "Null"
+
+    return Stock_in_criteria
+
+'''stocks= ("AC.MX", "ACTINVRB.MX", "AGUA.MX",
+         "ALFAA.MX", "ALPEKA.MX", "ALSEA.MX", "AMXB.MX", "ANB.MX", "ARA.MX", "ASURB.MX",
+         "AUTLANB.MX", "AXTELCPO.MX", "BBAJIOO.MX", "BBVA.MX", "BEVIDESB.MX", "BIMBOA.MX")
+
+lets_see= in_insutry(stocks, "Packaged Foods")
+print(lets_see)'''
